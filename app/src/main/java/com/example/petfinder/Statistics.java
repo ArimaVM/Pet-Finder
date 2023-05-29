e846cc63c30c520b8339a9536e52b72a3449b864
Arriana Mae Malubay <arimamalubay333@gmail.com>
2023-05-29 16:00:22 +0800

    Java bottomNav

--- app/src/main/java/com/example/petfinder/Statistics.java
+++ app/src/main/java/com/example/petfinder/Statistics.java
@@ -1,4 +1,12 @@
 package com.example.petfinder;
 
-public class Statistics {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Statistics extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+    }
 }
